8acaaf0d60ca3c77c1fea1768d997e65335abb3a
Luke Baker <akabaker@gmail.com>
2017-04-16 22:00:32 -0500

    added style

--- app/events/event-thumbnail.component.ts
+++ app/events/event-thumbnail.component.ts
@@ -16,7 +16,7 @@ import {Component,Input,} from '@angular/core'
     `,
     styles: [`
         .pad-left {margin-left: 10px;}
-        .well div {color: #000000;}
+        .well div {color: #bbb;}
     `]
 })
 export class EventThumbnailComponent {
