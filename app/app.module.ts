a5b7eb6ac2b67ccf07e567a4e6175aec3b19c1b7
Luke Baker <luke.baker@veteransunited.com>
2017-05-02 08:20:32 -0500

    Directives and modal pop-up

--- app/app.module.ts
+++ app/app.module.ts
@@ -19,12 +19,19 @@ import {
 
 import {EventsAppComponent} from './events-app.component'
 import {NavBarComponent} from './nav/navbar.component'
-import {TOASTR_TOKEN, Toastr} from './common/toastr.service'
-import {CollapsibleWellComponent} from './common/collapsible-well.component'
+import {
+    JQ_TOKEN,
+    TOASTR_TOKEN,
+    Toastr,
+    CollapsibleWellComponent,
+    SimpleModalComponent,
+    ModalTriggerDirective
+    } from './common/index'
 import {appRoutes} from './routes'
 import {Error404Component} from './errors/404.component'
 
 declare let toastr : Toastr
+declare let jQuery : Object;
 
 @NgModule({
     imports: [
@@ -44,12 +51,15 @@ declare let toastr : Toastr
         CreateSessionComponent,
         SessionListComponent,
         CollapsibleWellComponent,
-        DurationPipe
+        DurationPipe,
+        SimpleModalComponent,
+        ModalTriggerDirective
     ],
     providers: [
         EventsListResolver,
         EventService, 
         {provide: TOASTR_TOKEN, useValue: toastr}, 
+        {provide: JQ_TOKEN, useValue: jQuery}, 
         EventRouteActivator,
         AuthService,
         {provide: 'canDeactivateCreateEvent', useValue: checkDirtyState}
