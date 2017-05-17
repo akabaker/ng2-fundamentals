156e66461ef918bb0c752444221616b5d73801f2
Luke Baker <luke.baker@veteransunited.com>
2017-05-17 08:27:29 -0500

    Pushing and putting

--- app/events/event-details/voter.service.ts
+++ app/events/event-details/voter.service.ts
@@ -1,17 +1,35 @@
 import { Injectable } from '@angular/core';
 import { ISession } from "../index";
+import { Http, Response, Headers, RequestOptions } from '@angular/http';
+import { Observable } from "rxjs/Observable";
 
 @Injectable()
 export class VoterService {
-    deleteVoter(session: ISession, voterName: string) {
-        session.voters = session.voters.filter(voter => voter !== voterName);
+
+    constructor(private http: Http) {
+
     }
 
-    addVoter(session: ISession, voterName: string) {
-        session.voters.push(voterName);
+    deleteVoter(eventId: number, session: ISession, voterName: string) {
+        session.voters = session.voters.filter(voter => voter !== voterName);
+        let url = `/api/events/${eventId}/sessions/${session.id}/voters/${voterName}`;
+
+        this.http.delete(url).catch(this.handleError).subscribe();
+    }
+
+    addVoter(eventId: number, session: ISession, voterName: string) {
+        let headers = new Headers({'Content-Type': 'application/json'});
+        let options = new RequestOptions({headers: headers});
+        let url = `/api/events/${eventId}/sessions/${session.id}/voters/${voterName}`;
+
+        this.http.post(url, {}, options).catch(this.handleError).subscribe();
     }
 
     userHasVoted(session: ISession, voterName: string) {
         return session.voters.some(voter => voter === voterName);
+    }
+
+    private handleError(error: Response) {
+        return Observable.throw(error.statusText);
     }
 }
--- app/events/shared/event.service.ts
+++ app/events/shared/event.service.ts
@@ -1,7 +1,7 @@
 import { Injectable, EventEmitter } from '@angular/core'
 import { Subject, Observable } from 'rxjs/RX'
 import { IEvent, ISession } from './event.model'
-import { Http, Response } from '@angular/http'
+import { Http, Response, Headers, RequestOptions } from '@angular/http'
 
 @Injectable()
 export class EventService {
@@ -22,36 +22,19 @@ export class EventService {
         }).catch(this.handleError);
     }
 
-    saveEvent(event) {
-      event.id = 999
-      event.session = []
-      EVENTS.push(event)
-    }
+    saveEvent(event): Observable<IEvent> {
+        let headers = new Headers({'Content-Type': 'application/json'});
+        let options = new RequestOptions({headers: headers});
 
-    updateEvent(event) {
-      let index = EVENTS.findIndex(x => x.id = event.id)
-      EVENTS[index] = event
+        return this.http.post('/api/events', event, options).map((response: Response) => {
+          return response.json();
+        }).catch(this.handleError);
     }
 
     searchSessions(searchTerm: string) {
-      var term = searchTerm.toLocaleLowerCase();
-      var results: ISession[] = [];
-
-      EVENTS.forEach(event => {
-        var matchingSessions = event.sessions.filter(session => session.name.toLocaleLowerCase().indexOf(term) > -1);
-        matchingSessions = matchingSessions.map((session:any) => {
-          session.eventId = event.id;
-          return session;
-        });
-        results = results.concat(matchingSessions);
-      });
-
-      var emitter = new EventEmitter(true);
-      setTimeout(() => {
-        emitter.emit(results);
-      }, 100);
-
-      return emitter;
+        return this.http.get('/api/sessions/search?search=' + searchTerm).map((response:Response) => {
+            return response.json();
+        }).catch(this.handleError);
     }
 
     private handleError(error: Response) {
