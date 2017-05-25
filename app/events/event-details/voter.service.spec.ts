3692ed2d864b7932a8fb255616581d15efcaad8f
Luke Baker <luke.baker@veteransunited.com>
2017-05-24 19:47:05 -0500

    Unit tests with kerma done

--- app/events/event-details/voter.service.spec.ts
+++ app/events/event-details/voter.service.spec.ts
@@ -24,5 +24,31 @@ describe('VoterService', () => {
             expect(session.voters.length).toBe(1);
             expect(session.voters[0]).toBe("other");
         })
+
+        it('should call http.delete with the right url', () => {
+            var session = {
+                id: 6, 
+                voters: ["guy", "other"]
+            };
+            mockHttp.delete.and.returnValue(Observable.of(false));
+
+            voterService.deleteVoter(3, <ISession>session, "guy");
+
+            expect(mockHttp.delete).toHaveBeenCalledWith('/api/events/3/sessions/6/voters/guy');
+        })
+    })
+
+    describe('addVoter', () => {
+        it('should call http.post with the right url', () => {
+            var session = {
+                id: 6, 
+                voters: ["john"]
+            };
+            mockHttp.post.and.returnValue(Observable.of(false));
+
+            voterService.addVoter(3, <ISession>session, "john");
+
+            expect(mockHttp.post).toHaveBeenCalledWith('/api/events/3/sessions/6/voters/john', "{}", jasmine.any(Object));
+        })
     })
 })
